eb0b8728c77a4df565a6b8274b5073babaaaed51
Tom Green <kingofcuddles@gmail.com>
2014-08-01 13:33:05 +0100

    Swapped to tabbed view.
    Signed-off-by: Tom Green <kingofcuddles@gmail.com>

--- src/views/Admin/Post/Form.blade.php
+++ src/views/Admin/Post/Form.blade.php
@@ -8,58 +8,90 @@
     </h1>
     <ol class="breadcrumb">
       <li><a href="{{ route('dashboard') }}"><i class="fa fa-dashboard"></i> Dashboard</a></li>
-      <li><a href="{{ route('control.post.index') }}">Posts</a></li>
-      <li class="active">Edit</li>
+      <li><a href="{{ route('control.post.index') }}">Blog Posts</a></li>
+      <li class="active">Edit Blog Post</li>
     </ol>
   </section>
 
   <section class="content">
     <div class="row">
       <div class="col-md-12">
-        <div class="box {{ $errors->count() ? 'box-danger' : 'box-primary' }}">
-          <div class='box-header'>
-            <h3 class='box-title'>Editing Post: {{ $post->title }}</h3>
-            <div class="box-tools">
-              <a href="{{ route('control.post.destroy', $post->slug) }}" title="Delete {{ $post->title }}" class="btn btn-danger">Delete</a>
-            </div>
-          </div>
-          <div class="box-body">
+        <div class="nav-tabs-custom">
+          <ul class="nav nav-tabs pull-right">
+            <li><a href="#help" data-toggle="tab">Help</a></li>
+            <li><a href="#info" data-toggle="tab">Information</a></li>
+            <li><a href="#seo" data-toggle="tab">SEO</a></li>
+            <li class="active"><a href="#basic" data-toggle="tab">Basic</a></li>
+            <li class="pull-left header"><i class="fa fa-envelope"></i> Editing Post: {{ $post->title }}</li>
+            <li class="dropdown">
+              <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+              Actions <span class="caret"></span>
+              </a>
+              <ul class="dropdown-menu">
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="#"><i class="fa fa-eye"></i> Preview</a></li>
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="#"><i class="fa fa-bar-chart-o"></i> Statisctics</a></li>
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="#"><i class="fa fa-trash-o"></i> Delete</a></li>
+              </ul>
+            </li>
+          </ul>
+          <div class="tab-content">
+            <div class="tab-pane active" id="basic">
             @include('core::Admin/Partials/FlashMessages')
 
-          @if (isset($post))
-            {{ Form::model($post, ['method' => 'PUT', 'route' => ['control.post.update', $post->slug], 'role' => 'form']) }}
-            {{ Form::hidden('author_id', 1) }}
-          @else
-            {{ Form::open(['method' => 'POST', 'route' => 'control.post.store', 'role' => 'form']) }}
-            {{ Form::hidden('author_id', 1) }}
-          @endif
-            <div class="form-group {{ $errors->has('title') ? 'has-error' : '' }}">
-              {{ Form::label('title', 'Title') }}
-              {{ Form::text('title', null, ['class' => 'form-control']) }}
-              {{ $errors->first('title', '<p class="text-red">:message</p>') }}
+            @if (isset($post))
+              {{ Form::model($post, ['method' => 'PUT', 'route' => ['control.post.update', $post->slug], 'role' => 'form']) }}
+              {{ Form::hidden('author_id', 1) }}
+            @else
+              {{ Form::open(['method' => 'POST', 'route' => 'control.post.store', 'role' => 'form']) }}
+              {{ Form::hidden('author_id', 1) }}
+            @endif
+              <div class="form-group {{ $errors->has('title') ? 'has-error' : '' }}">
+                {{ Form::label('title', 'Title:') }}
+                {{ Form::text('title', null, ['class' => 'form-control']) }}
+                {{ $errors->first('title', '<p class="text-red">:message</p>') }}
+              </div>
+              <div class="form-group {{ $errors->has('excerpt') ? 'has-error' : '' }}">
+                {{ Form::label('excerpt', 'Excerpt:') }}
+                {{ Form::textarea('excerpt', null, ['class' => 'form-control', 'placeholder' => 'The Blog Post\'s excerpt', 'rows' => 5]) }}
+                {{ $errors->first('excerpt', '<p class="text-red">:message</p>') }}
+              </div>
+              <div class="form-group {{ $errors->has('content') ? 'has-error' : '' }}">
+                {{ Form::label('content', 'Content:') }}
+                {{ Form::textarea('content', null, ['class' => 'form-control', 'placeholder' => 'The Blog Post\'s content', 'rows' => 15]) }}
+                {{ $errors->first('content', '<p class="text-red">:message</p>') }}
+              </div>
             </div>
-            <div class="form-group {{ $errors->has('meta_title') ? 'has-error' : '' }}">
-              {{ Form::label('meta_title', 'META Title') }}
-              {{ Form::text('meta_title', null, ['class' => 'form-control']) }}
-              {{ $errors->first('meta_title', '<p class="text-red">:message</p>') }}
+            <div class="tab-pane" id="seo">
+              <div class="form-group {{ $errors->has('meta_title') ? 'has-error' : '' }}">
+                {{ Form::label('meta_title', 'META Title:') }}
+                {{ Form::text('meta_title', null, ['class' => 'form-control']) }}
+                {{ $errors->first('meta_title', '<p class="text-red">:message</p>') }}
+              </div>
+              <div class="form-group {{ $errors->has('meta_description') ? 'has-error' : '' }}">
+                {{ Form::label('meta_description', 'META Description:') }}
+                {{ Form::text('meta_description', null, ['class' => 'form-control']) }}
+                {{ $errors->first('meta_description', '<p class="text-red">:message</p>') }}
+              </div>
             </div>
-            <div class="form-group {{ $errors->has('meta_description') ? 'has-error' : '' }}">
-              {{ Form::label('meta_description', 'META Description') }}
-              {{ Form::text('meta_description', null, ['class' => 'form-control']) }}
-              {{ $errors->first('meta_description', '<p class="text-red">:message</p>') }}
+            <div class="tab-pane" id="info">
+              <div class="form-group">
+                {{ Form::label('created_at', 'Created at:') }}
+                {{ Form::text('created_at', null, ['class' => 'form-control', 'disabled' => 'disabled']) }}
+              </div>
+              <div class="form-group">
+                {{ Form::label('updated_at', 'Last updated:') }}
+                {{ Form::text('updated_at', null, ['class' => 'form-control', 'disabled' => 'disabled']) }}
+              </div>
             </div>
-            <div class="form-group {{ $errors->has('excerpt') ? 'has-error' : '' }}">
-              {{ Form::label('excerpt', 'Excerpt') }}
-              {{ Form::textarea('excerpt', null, ['class' => 'form-control', 'placeholder' => 'The Blog Post\'s excerpt', 'rows' => 5]) }}
-              {{ $errors->first('excerpt', '<p class="text-red">:message</p>') }}
+            <div class="tab-pane" id="help">
+              The European languages are members of the same family. Their separate existence is a myth.
+              For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ
+              in their grammar, their pronunciation and their most common words. Everyone realizes why a
+              new common language would be desirable: one could refuse to pay expensive translators. To
+              achieve this, it would be necessary to have uniform grammar, pronunciation and more common
+              words. If several languages coalesce, the grammar of the resulting language is more simple
+              and regular than that of the individual languages.
             </div>
-            <div class="form-group {{ $errors->has('content') ? 'has-error' : '' }}">
-              {{ Form::label('content', 'Content') }}
-              {{ Form::textarea('content', null, ['class' => 'form-control', 'placeholder' => 'The Blog Post\'s content', 'rows' => 15]) }}
-              {{ $errors->first('content', '<p class="text-red">:message</p>') }}
-            </div>
-          </div>
-          <div class="box-footer">
             {{ Form::submit('Save', ['class' => 'btn btn-primary']) }}
             {{ Form::close() }}
           </div>
--- src/views/Admin/Post/List.blade.php
+++ src/views/Admin/Post/List.blade.php
@@ -1,58 +1,75 @@
 @extends('core::Admin/Base')
 
 @section('content')
-<!-- Right side column. Contains the navbar and content of the page -->
 <aside class="right-side">
   <section class="content-header">
     <h1>
-      Blog
+      Blog Posts
     </h1>
     <ol class="breadcrumb">
       <li><a href="{{ route('dashboard') }}"><i class="fa fa-dashboard"></i> Dashboard</a></li>
-      <li><a href="{{ route('control.post.index') }}">Posts</a></li>
-      <li class="active">List</li>
+      <li><a href="{{ route('control.post.index') }}">Blog Posts</a></li>
+      <li class="active">Blog Post List</li>
     </ol>
   </section>
 
   <section class="content">
     <div class="row">
-      <div class="col-xs-12">
-        <div class="box">
-          <div class='box-header'>
-            <h3 class='box-title'>Post List</h3>
-          </div><!-- /.box-header -->
-          <div class="box-body table-responsive">
-
-            @include('core::Admin/Partials/FlashMessages')
-
-            <table id="posts" class="table table-bordered table-striped">
-              <thead>
-                <tr>
-                  <th>Title</th>
-                  <th>Status</th>
-                  <th>Author</th>
-                  <th>Last Update</th>
-                </tr>
-              </thead>
-              <tbody>
-              	@foreach($posts as $post)
-                <tr>
-                  <td><a href="{{ route('control.post.edit', $post->slug) }}" title="Edit {{ $post->title }}">{{ $post->title }}</a></td>
-                  <td>!!{{ $post->status }}!!</td>
-                  <td>!!{{ $post->author }}!!</td>
-                  <td>{{ $post->updated_at->diffForHumans() }}</td>
-                </tr>
-                @endforeach
-              </tbody>
-              <tfoot>
-                <tr>
-                  <th>Title</th>
-                  <th>Status</th>
-                  <th>Author</th>
-                  <th>Last Update</th>
-                </tr>
-              </tfoot>
-            </table>
+      <div class="col-md-12">
+        <div class="nav-tabs-custom">
+          <ul class="nav nav-tabs pull-right">
+            <li><a href="#help" data-toggle="tab">Help</a></li>
+            <li class="active"><a href="#basic" data-toggle="tab">Blog Posts</a></li>
+            <li class="pull-left header"><i class="fa fa-envelope"></i> Blog Post List</li>
+            <li class="dropdown">
+              <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+              Actions <span class="caret"></span>
+              </a>
+              <ul class="dropdown-menu">
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="{{ route('control.post.create') }}"><i class="fa fa-pencil-square-o"></i> New Blog Post</a></li>
+              </ul>
+            </li>
+          </ul>
+          <div class="tab-content">
+            <div class="tab-pane active" id="basic">
+              <table id="posts" class="table table-bordered table-striped">
+                <thead>
+                  <tr>
+                    <th>Title</th>
+                    <th>Status</th>
+                    <th>Author</th>
+                    <th>Last Update</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  @foreach($posts as $post)
+                  <tr>
+                    <td><a href="{{ route('control.post.edit', $post->slug) }}" title="Edit {{ $post->title }}">{{ $post->title }}</a></td>
+                    <td>!!{{ $post->status }}!!</td>
+                    <td>!!{{ $post->author }}!!</td>
+                    <td>{{ $post->updated_at->diffForHumans() }}</td>
+                  </tr>
+                  @endforeach
+                </tbody>
+                <tfoot>
+                  <tr>
+                    <th>Title</th>
+                    <th>Status</th>
+                    <th>Author</th>
+                    <th>Last Update</th>
+                  </tr>
+                </tfoot>
+              </table>
+            </div>
+            <div class="tab-pane" id="help">
+              The European languages are members of the same family. Their separate existence is a myth.
+              For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ
+              in their grammar, their pronunciation and their most common words. Everyone realizes why a
+              new common language would be desirable: one could refuse to pay expensive translators. To
+              achieve this, it would be necessary to have uniform grammar, pronunciation and more common
+              words. If several languages coalesce, the grammar of the resulting language is more simple
+              and regular than that of the individual languages.
+            </div>
           </div>
         </div>
       </div>
