796913b55f4c2306373bfa5351f086a8c902d523
Tom Green <kingofcuddles@gmail.com>
2014-08-04 14:38:51 +0100

    Cleanup.
    Signed-off-by: Tom Green <kingofcuddles@gmail.com>

--- src/SimpleCms/Blog/Post/Post.php
+++ src/SimpleCms/Blog/Post/Post.php
@@ -9,7 +9,7 @@ class Post extends BaseModel {
     'author_id' => 'numeric|required',
     'category_id' => 'numeric|required',
     'status' => 'numeric|required',
-    'slug' => 'alpha_dash|max:80|unique:posts,'. $this->slug,// unique:users,username,'.$id
+    'slug' => 'alpha_dash|max:80|unique:posts',
     'meta_title' => 'max:70',
     'meta_description' => 'max:155',
     'title' => 'max:100|required',
