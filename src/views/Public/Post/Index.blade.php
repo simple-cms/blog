e5b3e8f5922038e137205618caa6fe8314f531e0
Tom Green <kingofcuddles@gmail.com>
2015-01-29 18:58:18 +0000

    cleaned up front end tpls a little bit.
    Signed-off-by: Tom Green <kingofcuddles@gmail.com>

--- src/views/Public/Post/Index.blade.php
+++ src/views/Public/Post/Index.blade.php
@@ -1,10 +1,11 @@
 @extends('core::Public/Base')
 
 @section('content')
-  @foreach ($posts as $post)
+<div class="container">
   <div class="row">
-    <div class="span12">
-      <a href='{{ route('blog.show', $post->slug) }}' title='{{ $post->title }}'><h2>{{ $post->title }}</h2></a>
+    @foreach ($posts as $post)
+    <div class="col-md-12">
+      <a href='{{ route(config('post.postURL') .'.show', $post->slug) }}' title='{{ $post->title }}'><h2>{{ $post->title }}</h2></a>
       <p><small>Posted
         @if ($post->category !== null)
         in {{ $post->category->title }}
@@ -16,8 +17,7 @@
       </small></p>
       <p>{{ $post->excerpt }}</p>
     </div>
+    @endforeach
   </div>
-  <hr />
-  @endforeach
-
+</div>
 @stop
--- src/views/Public/Post/Show.blade.php
+++ src/views/Public/Post/Show.blade.php
@@ -1,17 +1,23 @@
 @extends('core::Public/Base')
 
 @section('content')
-  <h2>{{ $post->title }}</h2>
-  <p><small>Posted
-    @if ($post->category !== null)
-    in {{ $post->category->title }}
-    @endif
-    on {{ $post->created_at }}
-    @if ($post->updated_at != '0000-00-00 00:00:00')
-    (last updated at {{ $post->updated_at }})
-    @endif
-  </small></p>
-  <blockquote>{{ $post->excerpt }}</blockquote>
-  {{ $post->content }}
-  <hr />
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <h2>{{ $post->title }}</h2>
+      <p><small>Posted
+        @if ($post->category !== null)
+        in {{ $post->category->title }}
+        @endif
+        on {{ $post->created_at }}
+        @if ($post->updated_at != '0000-00-00 00:00:00')
+        (last updated at {{ $post->updated_at }})
+        @endif
+      </small></p>
+      <blockquote>{{ $post->excerpt }}</blockquote>
+      {{ $post->content }}
+      <hr />
+    </div>
+  </div>
+</div>
 @stop
