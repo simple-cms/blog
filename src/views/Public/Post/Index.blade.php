3ccbeaedf257c6d1d1083088b96abae5dd354667
Tom Green <kingofcuddles@gmail.com>
2015-01-29 19:23:54 +0000

    further cleaning of base templates.
    Signed-off-by: Tom Green <kingofcuddles@gmail.com>

--- src/views/Public/Post/Index.blade.php
+++ src/views/Public/Post/Index.blade.php
@@ -6,15 +6,6 @@
     @foreach ($posts as $post)
     <div class="col-md-12">
       <a href='{{ route(config('post.postURL') .'.show', $post->slug) }}' title='{{ $post->title }}'><h2>{{ $post->title }}</h2></a>
-      <p><small>Posted
-        @if ($post->category !== null)
-        in {{ $post->category->title }}
-        @endif
-        on {{ $post->created_at }}
-        @if ($post->updated_at != '0000-00-00 00:00:00')
-        (last updated at {{ $post->updated_at }})
-        @endif
-      </small></p>
       <p>{{ $post->excerpt }}</p>
     </div>
     @endforeach
--- src/views/Public/Post/Show.blade.php
+++ src/views/Public/Post/Show.blade.php
@@ -5,17 +5,9 @@
   <div class="row">
     <div class="col-md-12">
       <h2>{{ $post->title }}</h2>
-      <p><small>Posted
-        @if ($post->category !== null)
-        in {{ $post->category->title }}
-        @endif
-        on {{ $post->created_at }}
-        @if ($post->updated_at != '0000-00-00 00:00:00')
-        (last updated at {{ $post->updated_at }})
-        @endif
-      </small></p>
+      <p><small>{{ $post->category !== null ? '<i class="fa fa-archive"></i> '. $post->category->title .'.' : '' }} <i class="fa fa-clock-o"></i> {{ $post->created_at->format('jS F Y') }}</small></p>
       <blockquote>{{ $post->excerpt }}</blockquote>
-      {{ $post->content }}
+      {!! $post->content !!}
       <hr />
     </div>
   </div>
