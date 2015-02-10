6d9a7c611235487266e2f54898926c4c6b3e7294
Tom Green <kingofcuddles@gmail.com>
2015-02-10 15:54:49 +0000

    fixed issue with routes
    Signed-off-by: Tom Green <kingofcuddles@gmail.com>

--- src/routes.php
+++ src/routes.php
@@ -5,6 +5,6 @@ Route::resource(config('category.categoryURL'), 'SimpleCms\Blog\Category\PublicC
 
 Route::group(['prefix' => config('core.adminURL')], function()
 {
-  Route::resource('post', 'SimpleCms\Blog\Post\AdminController', ['only' => ['index', 'create', 'store', 'edit', 'update', 'destroy']]);
-  Route::resource('category', 'SimpleCms\Blog\Category\AdminController', ['only' => ['index', 'create', 'store', 'edit', 'update', 'destroy']]);
+  Route::resource(config('post.postURL'), 'SimpleCms\Blog\Post\AdminController', ['only' => ['index', 'create', 'store', 'edit', 'update', 'destroy']]);
+  Route::resource(config('category.categoryURL'), 'SimpleCms\Blog\Category\AdminController', ['only' => ['index', 'create', 'store', 'edit', 'update', 'destroy']]);
 });
